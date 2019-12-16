9d8d2fa6c58cb453f1ce6cce6fcdb461b7753ca5
Noah <sheltielover432@gmail.com>
2019-12-16 00:15:05 -0500

    Shooting projectiles

--- zombie/main.lua
+++ zombie/main.lua
@@ -9,6 +9,7 @@ function love.load(arg)
   player.y = 200
   player.speed = 180
   zombies = {}
+  bullets = {}
 end
 
 function love.update(dt)
@@ -34,6 +35,36 @@ function love.update(dt)
       end
     end
   end
+  for i,b in ipairs(bullets) do
+    b.x = b.x + math.cos(b.direction) * b.speed * dt
+    b.y = b.y + math.cos(b.direction) * b.speed * dt
+  end
+  for i=#bullets, 1, -1 do
+    local b = bullets[i]
+    if b.x < 0 or b.y < 0 or b.x > love.graphics:getWidth() or b.y > love.graphics:getHeight() then
+      table.remove(bullets, i)
+    end
+  end
+  for i,z in ipairs(zombies) do
+    for j,b in pairs(bullets) do
+      if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+        z.dead = true
+        b.dead = true
+      end
+    end
+  end
+  for i=#zombies,1,-1 do
+    local z = zombies[i]
+    if z.dead == true then
+      table.remove(zombies, i)
+  end
+end
+for i=#bullets,1,-1 do
+  local b = bullets[i]
+  if b.dead == true then
+    table.remove(bullets, i)
+    end
+  end
 end
 
 function love.draw()
@@ -41,6 +72,9 @@ function love.draw()
   love.graphics.draw(sprites.player, player.x, player.y, player_mouse_angle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
   for i,z in ipairs(zombies) do
       love.graphics.draw(sprites.zombie, z.x, z.y, zombie_player_angle(z), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
+  end
+  for i, b in ipairs(bullets) do
+    love.graphics.draw(sprites.bullet, b.x, b.y, nil, 0.5, 0.5, sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2)
   end
 end
 
@@ -56,13 +90,31 @@ function spawnZombie()
   zombie = {}
   zombie.x = math.random(0, love.graphics:getWidth())
   zombie.y = math.random(0, love.graphics:getHeight())
-  zombie.speed = 130
+  zombie.speed = 140
+  zombie.dead = false
   table.insert(zombies, zombie)
+  local side = math.math.random(1, 4)
+end
+
+function spawnBullet()
+  bullet = {}
+  bullet.x = player.x
+  bullet.y = player.y
+  bullet.speed = 500
+  bullet.direction = player_mouse_angle()
+  bullet.dead = false
+  table.insert(bullets, bullet)
 end
 
 function love.keypressed(key, scancode, isrepeat)
   if key == "space" then
     spawnZombie()
+  end
+end
+
+function love.mousepressed(x, y, button, isTouch)
+  if b==1 then
+    spawnBullet()
   end
 end
 
